37e66b916afa119552080d14a6aee7ea872b74a1
Anton P. Linevich <anton@linevich.com>
2017-11-17 18:27:30 +0200

    use constants for test resource names

--- src/test/java/com/vg/live/SuperLiveTest.java
+++ src/test/java/com/vg/live/SuperLiveTest.java
@@ -18,10 +18,13 @@ import rx.Observable;
 
 public class SuperLiveTest {
 
+    public static final String M4S = "testdata/trun/stream-0.42.m4s";
+    public static final String M4S_INIT = "testdata/trun/stream-0.0.m4s";
+
     @Test
     public void sampleCount() {
-        File init = new File("testdata/trun/stream-0.0.m4s");
-        File m4s = new File("testdata/trun/stream-0.42.m4s");
+        File init = new File(M4S_INIT);
+        File m4s = new File(M4S);
         File file = new File("testdata/trun/stream-0.116.m4s");
 
         Observable<ByteBuffer> bufs = just(init, m4s, file).map(f1(f -> {
@@ -36,8 +39,8 @@ public class SuperLiveTest {
 
     @Test
     public void duration() {
-        File init = new File("testdata/trun/stream-0.0.m4s");
-        File m4s = new File("testdata/trun/stream-0.42.m4s");
+        File init = new File(M4S_INIT);
+        File m4s = new File(M4S);
 
         Observable<ByteBuffer> bufs = just(init, m4s).map(f1(f -> {
             System.out.println("f=" + f.getAbsolutePath());
